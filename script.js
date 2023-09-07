04ace6411fa4e688a124a338b817e8cd624e497b
Rajat  Singh <129305916+raja12351@users.noreply.github.com>
2023-09-08 05:23:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	return fibo(num);
+}
+
+function fibo(num){
+	if(num == 0){
+		return 0;
+	}
+	if( num == 1){
+		return 1;
+	}
+
+	return fibo(num - 1) + fibo(num - 2);
 }
 
 module.exports = fibonacci;
